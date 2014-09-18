c59e6db998cf16f5c961d1419aa0b780732e6f64
amichels <andy.j.michels@gmail.com>
2014-09-18 18:31:27 -0500

    validation

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,57 @@
 <?php
+// app/routes.php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+// route to show the duck form
+Route::get('ducks', function(){
+	return View::make('duck-form');
+});
 
-Route::get('/', function()
-{
-	return View::make('hello');
+// route to process the ducks form
+Route::post('ducks', function(){
+
+	// process the form here
+
+	// create the validation rules ------------------------
+	$rules = array(
+		'name'             => 'required', 						// just a normal required validation
+		'email'            => 'required|email|unique:ducks', 	// required and must be unique in the ducks table
+		'password'         => 'required',
+		'password_confirm' => 'required|same:password' 			// required and has to match the password field
+	);
+
+	// do the validation ----------------------------------
+	// validate against the inputs from our form
+	$validator = Validator::make(Input::all(), $rules);
+
+	// check if the validator failed -----------------------
+	if ($validator->fails()) {
+
+		// get the error messages from the validator
+		$messages = $validator->messages();
+
+		// redirect our user back to the form with the errors from the validator
+		return Redirect::to('ducks')
+			->withErrors($validator);
+
+	} else {
+		// validation successful ---------------------------
+
+		// our duck has passed all tests!
+		// let him enter the database
+
+		// create the data for our duck
+		$duck = new Duck;
+		$duck->name     = Input::get('name');
+		$duck->email    = Input::get('email');
+		$duck->password = Hash::make(Input::get('password'));
+
+		// save our duck
+		$duck->save();
+
+		// redirect ----------------------------------------
+		// redirect our user back to the form so they can do it all over again
+		return Redirect::to('ducks');
+
+	}
+
 });
